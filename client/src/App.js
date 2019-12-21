69b7f55d313033b9846479b6b8220e6a824848df
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-12-21 00:09:24 -0500

    Added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.scss';
+import Main from './components/MainPage/Main';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Main />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
