505c0b15de1c28d0d712fa3f781c91eae1324af4
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-01-25 16:59:12 -0500

    Connected reigster form with formik

--- client/src/components/MainPage/Register/Register.js
+++ client/src/components/MainPage/Register/Register.js
@@ -1,82 +1,23 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Form from 'react-bootstrap/Form'
-import InputGroup from 'react-bootstrap/InputGroup'
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
-import Button from 'react-bootstrap/Button'
 import './Register.scss'
+import RegisterForm from './RegisterForm'
 
-export default class Register extends PureComponent {
-    render() {
-        const { container, setContainer } = this.props
+export default function Register({ container, setContainer }) {
+    return (
+        <div className={(container === 1) ? 'register-container home-cards show-register' : (container === 0) ? 'register-container home-cards hide-register' : 'register-container home-cards'}>
+            <Row className="text-center mt-4 mb-4">
+                <Col xs="12">
+                    <h1>REGISTER</h1>
+                </Col>
+            </Row>
 
-        return (
-            <div className={(container === 1) ? 'register-container home-cards show-register' : (container === 0) ? 'register-container home-cards hide-register' : 'register-container home-cards'}>
-                <Row className="text-center mt-4 mb-4">
-                    <Col xs="12">
-                        <h1>REGISTER</h1>
-                    </Col>
-                </Row>
-
-                <Row>
-                    <Col xs="12">
-                        <Form>
-                            <Row>
-                                <Col xs="12" md={{ span: 10, offset: 1 }}>
-                                    <Form.Group>
-                                        <InputGroup className="register-inputs home-inputs">
-                                            <InputGroup.Prepend>
-                                                <InputGroup.Text><i className="fas fa-user"></i></InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                            <Form.Control type="text" placeholder="Enter username..." />
-                                        </InputGroup>
-                                    </Form.Group>
-
-                                    <Form.Group>
-                                        <InputGroup className="register-inputs home-inputs">
-                                            <InputGroup.Prepend>
-                                                <InputGroup.Text><i className="fas fa-at"></i></InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                            <Form.Control type="email" placeholder="Enter email..." />
-                                        </InputGroup>
-                                    </Form.Group>
-
-
-                                    <Form.Group className="mb-3">
-                                        <InputGroup className="login-inputs home-inputs">
-                                            <InputGroup.Prepend>
-                                                <InputGroup.Text>
-                                                    <i className="fas fa-key"></i>
-                                                </InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                            <Form.Control type="password" placeholder="Enter password..." />
-                                        </InputGroup>
-                                    </Form.Group>
-
-                                    <Form.Group className="mb-3">
-                                        <InputGroup className="login-inputs home-inputs">
-                                            <InputGroup.Prepend>
-                                                <InputGroup.Text>
-                                                    <i className="fas fa-shield-alt"></i>
-                                                </InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                            <Form.Control type="password" placeholder="Re-enter password..." />
-                                        </InputGroup>
-                                    </Form.Group>
-                                </Col>
-                            </Row>
-
-                            <Form.Group className="m-0">
-                                <ButtonGroup className="register-buttons home-buttons">
-                                    <Button onClick={() => setContainer(0)}><i className="fas fa-arrow-left"></i> &nbsp; BACK</Button>
-                                    <Button>SUBMIT</Button>
-                                </ButtonGroup>
-                            </Form.Group>
-                        </Form>
-                    </Col>
-                </Row>
-            </div>
-        )
-    }
+            <Row>
+                <Col xs="12">
+                    <RegisterForm setContainer={setContainer} />
+                </Col>
+            </Row>
+        </div>
+    )
 }
---
+++ client/src/components/MainPage/Register/RegisterForm.js
@@ -0,0 +1,123 @@
+import React from 'react'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Button from 'react-bootstrap/Button'
+import FormGroup from 'react-bootstrap/FormGroup'
+import Alert from 'react-bootstrap/Alert'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+
+const RegisterForm = ({ setContainer }) => {
+    const RegisterSchema = Yup.object().shape({
+        register: Yup.object().shape({
+            username: Yup.string().trim().lowercase()
+                .required('Username is required')
+                .min(3, 'Username must be longer than 3 characters')
+                .max(10, 'Username must be shorter than 10 characters'),
+            email: Yup.string().trim()
+                .email("Email must be valid")
+                .notRequired(),
+            password: Yup.string()
+                .required('Password is required')
+                .min(6, 'Password must be longer than 6 characters')
+                .max(12, 'Password must be shorter than 12 characters')
+                .matches(/^(?=.*[a-z])/, "Password must contain at least 1 lowercase letter")
+                .matches(/^(?=.*[A-Z])/, "Password must contain at least 1 uppercase letter")
+                .matches(/^(?=.*[0-9])/, "Password must contain at least 1 number"),
+            passwordConf: Yup.string()
+                .required('Please re-enter password')
+                .oneOf([Yup.ref('password')], 'Passwords must match'),
+        }),
+    })
+
+    return (
+        <Formik
+            initialValues={{
+                register: {
+                    username: '',
+                    email: '',
+                    password: '',
+                    passwordConf: '',
+                }
+            }}
+
+            validationSchema={RegisterSchema}
+
+            onSubmit={
+                (values, { setSubmitting }) => {
+                    setSubmitting(false)
+                    console.log(values)
+                }
+            }
+        >
+
+            {({ isSubmitting, resetForm }) => (
+                <Form>
+                    <Row>
+                        <Col xs="12" md={{ span: 10, offset: 1 }}>
+                            <FormGroup>
+                                <ErrorMessage name="register.username">{msg => <Alert className="mb-1 form-error text-center" variant="danger">{msg}</Alert>}</ErrorMessage>
+
+                                <InputGroup className="register-inputs home-inputs">
+                                    <InputGroup.Prepend>
+                                        <InputGroup.Text><i className="fas fa-user"></i></InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <Field className="form-control" type="text" name="register.username" placeholder="Enter username...." />
+                                </InputGroup>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <ErrorMessage name="register.email">{msg => <Alert className="mb-1 form-error text-center" variant="danger">{msg}</Alert>}</ErrorMessage>
+
+                                <InputGroup className="register-inputs home-inputs">
+                                    <InputGroup.Prepend>
+                                        <InputGroup.Text><i className="fas fa-at"></i></InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <Field className="form-control" type="email" name="register.email" placeholder="Enter email..." />
+                                </InputGroup>
+                            </FormGroup>
+
+
+                            <FormGroup className="mb-3">
+                                <ErrorMessage name="register.password">{msg => <Alert className="mb-1 form-error text-center" variant="danger">{msg}</Alert>}</ErrorMessage>
+
+                                <InputGroup className="login-inputs home-inputs">
+                                    <InputGroup.Prepend>
+                                        <InputGroup.Text>
+                                            <i className="fas fa-key"></i>
+                                        </InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <Field className="form-control" type="password" name="register.password" placeholder="Enter password..." />
+                                </InputGroup>
+                            </FormGroup>
+
+                            <FormGroup className="mb-4">
+                                <ErrorMessage name="register.passwordConf">{msg => <Alert className="mb-1 form-error text-center" variant="danger">{msg}</Alert>}</ErrorMessage>
+
+                                <InputGroup className="login-inputs home-inputs">
+                                    <InputGroup.Prepend>
+                                        <InputGroup.Text>
+                                            <i className="fas fa-shield-alt"></i>
+                                        </InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <Field className="form-control" type="password" name="register.passwordConf" placeholder="Re-enter password..." />
+                                </InputGroup>
+                            </FormGroup>
+                        </Col>
+                    </Row>
+
+                    <FormGroup className="m-0">
+                        <ButtonGroup className="register-buttons home-buttons">
+                            <Button onClick={() => { setContainer(0); resetForm() }}><i className="fas fa-arrow-left"></i> &nbsp; BACK</Button>
+                            <Button type="submit" disabled={isSubmitting}>SUBMIT</Button>
+                        </ButtonGroup>
+                    </FormGroup>
+                </Form>
+            )}
+        </Formik>
+    )
+}
+
+export default RegisterForm
